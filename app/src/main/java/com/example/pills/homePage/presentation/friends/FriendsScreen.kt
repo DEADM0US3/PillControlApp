d71ec6f55c358dca19af547c39632ddc766d2035
DEADM0US3 <ffernando.villafanaalfonseca@gmail.com>
2025-07-18 17:54:04 -0600

    doing(friends): Initial part of the list friends page

---
+++ app/src/main/java/com/example/pills/homePage/presentation/friends/FriendsScreen.kt
@@ -0,0 +1,12 @@
+package com.example.pills.homePage.presentation.friends
+
+import androidx.compose.runtime.Composable
+import com.example.pills.homePage.components.FriendsListSectionUI
+
+@Composable
+fun FriendsScreen(
+
+)
+{
+    FriendsListSectionUI()
+}
